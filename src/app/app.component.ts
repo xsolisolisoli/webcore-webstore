fca5dc7c1184bba7d2b2687b4ff36bc7496a109a
soli <69920747+xsolisolisoli@users.noreply.github.com>
2025-02-21 11:05:26 -0600

    verify popup component is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PopupComponent } from "./popup/popup.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PopupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
